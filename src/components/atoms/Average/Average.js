e939681121fde0c9ed8c555381bce1b6626dff54
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-06 10:20:55 +0100

    Delete src directory

--- src/components/atoms/Average/Average.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AverageStyle } from "./Average.styles";
-
-export let averageColor;
-
-export const Average2 = (props) => (
-  <AverageStyle>{(averageColor = props.marian)}</AverageStyle>
-);
--- src/components/organisms/UsersList/UsersList.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
-  width: 50%;
-  padding: 40px 30px;
-  border-radius: 25px;
-  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
-`;
