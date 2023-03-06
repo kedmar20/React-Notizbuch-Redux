ef6b85f7bc658f45ca0645992611013f88ea5333
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-06 20:15:27 +0100

    stage

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -2,15 +2,43 @@ import React from "react";
 import { users } from "data/users";
 import UsersListItem from "components/molecules/UsersListItem/UsersListItem";
 import { StyledList, Wrapper } from "./UsersList.styles";
+import Button from "components/atoms/Button/Button";
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} key={userData.name} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+// const UsersList = () => (
+//   <Wrapper>
+//     <StyledList>
+//       {users.map((userData, i) => (
+//         <UsersListItem index={i} key={userData.name} userData={userData} />
+//       ))}
+//     </StyledList>
+//   </Wrapper>
+// );
+
+//Klasy, rys historyczny dot stanu:
+class UsersList extends React.Component {
+  state = {
+    isUsersList: true,
+  };
+
+  toggleListTitle = () => {
+    this.setState((prevState) => ({
+      isUsersList: !prevState.isUsersList,
+    }));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.isUsersList ? `User's list` : "Students list"}</h1>
+        <button onClick={this.toggleListTitle}>Change title</button>
+        <StyledList>
+          {users.map((userData, i) => (
+            <UsersListItem index={i} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
