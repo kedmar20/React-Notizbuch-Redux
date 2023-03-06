4de143555274cc331cdcf973845ee001543deb51
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-06 12:20:41 +0100

    Update Button.js

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -3,7 +3,7 @@ import { ReactComponent as DeleteIcon } from "assets/icons/icon_white.svg";
 import { StyledButton } from "./Button.styles";
 
 const Button = (props) => (
-  <StyledButton {...props}>
+  <StyledButton {...props}> 
     <DeleteIcon />
   </StyledButton>
 );
