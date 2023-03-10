806e6af372970d4fe13a8e5909cf9106a889248f
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-10 04:42:59 +0100

    add new views

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,76 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { users as MKusersData } from "data/users";
+import React from "react";
 import UsersListItem from "components/molecules/UsersListItem/UsersListItem";
-import { StyledList, StyledTitle, Wrapper } from "./UsersList.styles";
-import FormField from "components/molecules/FormField/FormField";
-import { Button } from "components/atoms/Button/Button";
+import { StyledList, Wrapper } from "./UsersList.styles";
+import { UserShape } from "types";
+import { Title } from "components/atoms/Title/Title";
+import PropTypes from "prop-types";
 
-const initialFormState = {
-  name: "",
-  attendance: "",
-  average: "",
-};
-
-const UsersList = () => {
-  const [usersMK, setUsers] = useState(MKusersData); //początkowe zaczytanie danych z jakiegoś źródła; egal jakiego
-  const [formValues, setFormValues] = useState(initialFormState); // control component / przekazujemy: 1.wartośćDoInputu (początkowo obiekt kluczy z pustymi wartościami) poprzez funkcję nr 1 czyli w tym przypadku 'formValues' jako props niżej do dziecka gdzie wyświetla się input czyli do 'FormField' / i nastepnie jesli nastąpi jakaś zmiana (event 'onChange') to poprze funkcję nr 2 czyli w tym przypadku 'setFormValues' od dziecka do Stanu dostajemy nową wartość / i tak wkołomacieju
-
-  const deleteUser = (name) => {
-    const filteredUsers = usersMK.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleInputChange = (e) => {
-    setFormValues({
-      ...formValues, //zaczytanie aktualnego stanu
-      [e.target.name]: e.target.value, //jeśli odpala się event onChange to klucz o takim name przyjmuje nową wartość
-    });
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault(); //zapobiega przeładowanu strony
-    const newUser = {
-      name: formValues.name,
-      attendanec: formValues.attendance,
-      average: formValues.average,
-    };
-    setUsers([newUser, ...usersMK]); //ustawiamy nową listę userów
-    setFormValues(initialFormState);
-  };
-
+const UsersList = ({ users, deleteUser }) => {
   return (
     <>
-      <Wrapper as="form" onSubmit={handleAddUser}>
-        {" "}
-        {/*//czyli jeśli dojdzie do submit to ma się wykonać funkcja*/}
-        <StyledTitle>Neuen Studenten hinzufügen:</StyledTitle>
-        <FormField
-          label="Nachname:"
-          id="name"
-          name="name"
-          value={formValues.name}
-          onChange={handleInputChange}
-        />
-        <FormField
-          label="Teilnahmefrequenz:"
-          id="attendance"
-          name="attendance"
-          value={formValues.attendance}
-          onChange={handleInputChange}
-        />
-        <FormField
-          label="Durchschnitt:"
-          id="average"
-          name="average"
-          value={formValues.average}
-          onChange={handleInputChange}
-        />
-        <Button type="submit">Add</Button>
-      </Wrapper>
       <Wrapper>
-        <StyledTitle>Studentenliste:</StyledTitle>
+        <Title>Studentenliste:</Title>
         <StyledList>
-          {usersMK.map((userDataMK) => (
+          {users.map((userDataMK) => (
             <UsersListItem
               deleteUser={deleteUser}
               key={userDataMK.name}
@@ -81,6 +22,11 @@ const UsersList = () => {
       </Wrapper>
     </>
   );
+};
+
+UsersList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape(UserShape)),
+  deleteUser: PropTypes.func,
 };
 
 export default UsersList;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,17 +1,79 @@
-import React from "react";
-import UsersList from "components/organisms/UsersList/UsersList";
+import React, { useState } from "react";
+import { users as MKusersData } from "data/users";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "assets/styles/GlobalStyle";
 import { theme } from "assets/styles/theme";
 import { Wrapper } from "./Root.styles";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainTemplate from "components/templates/MainTemplate/MainTemplate";
+import AddUser from "./AddUser";
+import Dashboard from "./Dashboardd";
+//
+//
+const initialFormState = {
+  name: "",
+  attendance: "",
+  average: "",
+};
+//
+//
+//
+const Root = () => {
+  const [users, setUsers] = useState(MKusersData);
+  const [formValues, setFormValues] = useState(initialFormState);
 
-const Root = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Wrapper>
-      <UsersList />
-    </Wrapper>
-  </ThemeProvider>
-);
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: formValues.name,
+      attendanec: formValues.attendance,
+      average: formValues.average,
+    };
+    setUsers([newUser, ...users]);
+    setFormValues(initialFormState);
+  };
+
+  return (
+    <Router>
+      {/* //zapewnia dla całej aplikacji możliwość routingu czyli przemieszczania sie pomiędzy stronami */}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <MainTemplate>
+          <Wrapper>
+            <Routes>
+              <Route
+                path="add-user"
+                element={
+                  <AddUser
+                    handleAddUser={handleAddUser}
+                    handleInputChange={handleInputChange}
+                    formValues={formValues}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/"
+                exact
+                element={<Dashboard deleteUser={deleteUser} users={users} />}
+              ></Route>
+            </Routes>
+          </Wrapper>
+        </MainTemplate>
+      </ThemeProvider>
+    </Router>
+  );
+};
 
 export default Root;
