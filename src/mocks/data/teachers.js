6f42b2ffa222bd565e968687f550c82e6c50514c
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-16 18:16:23 +0100

    add new data

--- src/mocks/data/teachers.js
+++ src/mocks/data/teachers.js
@@ -1,8 +1,14 @@
 export const teachers = [
-  {
-    name: 'Jacek Sobczak',
-    subjects: ['Modern economy', 'Business Philosophy'],
-    login: 'j.sobczak@react.com',
-    password: '1234',
-  },
+   {
+      name: "Jacek Sobczak",
+      subjects: ["Modern economy", "Business Philosophy"],
+      login: "j.sobczak@react.com",
+      password: "1234",
+   },
+   {
+      name: "Olga Schiefer",
+      subjects: ["Modern economy", "Business Philosophy"],
+      login: "o.schiefer@react.com",
+      password: "1234",
+   },
 ];
